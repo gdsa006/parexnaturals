5b9ac6225734a23206e309b889f96821e8d5e0e2
beyond-engg <projects.mvut@gmail.com>
2017-06-21 12:09:40 +0530

    as

---
+++ location.js
@@ -0,0 +1,140 @@
+﻿	function getUrlVars() {
+	    var vars = [], hash;
+	    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	    for (var i = 0; i < hashes.length; i++) {
+	        hash = hashes[i].split('=');
+	        vars.push(hash[0]);
+	        vars[hash[0]] = hash[1];
+	    }
+	    return vars;
+	}
+function OpenMapModal(title, link) {
+
+    $('#dvDesc').html($(link).parents('.location').find('.desc').html());
+
+    $('#myModalLabel1').html(title);
+
+    $('#mdLocation').modal('show');
+    var address = $(link).parents('.location').find('address').find('span').text();
+    GetLocationAndSetPin(address);
+}
+
+$('#mdLocation').on('shown.bs.modal', function (e) {
+    RefreshGooglemap();
+})
+$(function () {
+    //change page title. when come from 
+    var title = getUrlVars()["pt"];
+    if (title == "reqapp") {
+        $(".page-title").html("Request an Appointment");
+        
+        $(".req-a").removeClass("req-app").addClass("btn-thm");
+    } else if (title == "lam") {
+        $(".page-title").html("Leave a Message");
+
+        $(".req-m").removeClass("req-app").addClass("btn-thm");
+    }
+
+
+    var selectLoc = $('#' + getUrlVars()["loc"]);
+
+    var docWidth = $(document).width();
+    var scrollalign;
+    if (docWidth < 767) {
+        scrollalign = -10;
+    }
+    else {
+        scrollalign = -150;
+    }
+
+    if (selectLoc.length != 0) {
+        $('html, body').animate({
+            scrollTop: selectLoc.offset().top + scrollalign
+        }, 2000, null, function () {
+            selectLoc.addClass('active-loc');
+            setTimeout(function () {
+                selectLoc.find('.linkTwo').trigger("click");
+            }, 500);
+        });
+    }
+});
+
+$('#mdLocation').on('hidden.bs.modal', function (e) {
+    $('.active-loc').removeClass('active-loc');
+});
+
+
+var fromLat = 39.9689946, fromLng = -75.58809659999997, myLatlng, map;
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+function GetLocationAndSetPin(address) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            fromLat = results[0].geometry.location.lat();
+            fromLng = results[0].geometry.location.lng();
+            myLatlng = new google.maps.LatLng(fromLat, fromLng);
+            //console.log(fromLat);
+            //console.log(fromLng);
+
+            initialize();
+
+        } else {
+            alert("Request failed.")
+        }
+    });
+};
+
+function initialize() {
+    var markers = [];
+    myLatlng = new google.maps.LatLng(fromLat, fromLng);
+    var mapOptions = {
+        zoom: 12,
+        center: myLatlng
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+
+    var input = (document.getElementById('pac-input'));
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
+        CalcRoute();
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Destimation'
+    });
+}
+function CalcRoute() {
+    var start = document.getElementById('pac-input').value;
+    var end = new google.maps.LatLng(fromLat, fromLng);
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
+    });
+}
+
+function RefreshGooglemap() {
+   // try {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+
+    //}
+   // catch (ex) {
+     //   console.log(ex);
+   // }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ request.js
@@ -0,0 +1,76 @@
+﻿var hostPath = 'https://www.affordascan.com';
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+var qpage = getUrlVars()["pt"];
+var qlocationId = getUrlVars()["lid"];
+
+var pageNames = [
+           {
+               name: 'reqaap', pageName: 'radiology-app-request-appointment'
+               , device: { iphone: 1409, ipad: { p: 1223, l: 883 }, desktop: 900 }
+           }
+           , {
+               name: 'lam', pageName: "radiology-app-send-message"
+               , device: { iphone: 1198, ipad: { p: 1079, l: 730 }, desktop: 747 }
+           }
+
+           , { name: 'TODO', pageName: "radiology-app-location" }
+];
+
+var pageName;
+$.each(pageNames, function (idx, obj) {
+    if (obj.name == qpage) {
+        pageName = obj.pageName;
+        document.getElementById('cmd_iframe').height = findDevice(obj.device);
+        return false;
+    }
+});
+if (pageName == undefined) {
+    alert("Some error. Please try again after some time.");
+}
+var panelColor = "143F90";
+var themeName = localStorage.getItem('ThemeName');
+
+if (themeName == "blue") {
+    panelColor = "143F90"
+} else if (themeName == "green") {
+    panelColor = "71AA25"
+}
+
+var url = hostPath + "/jsp/" + pageName + ".html?locationId=" + qlocationId + "&noh=y" + "&c=" + panelColor;
+document.getElementById('cmd_iframe').src = url;
+
+function findDevice(obj) {
+    var width = $(document).width();
+    if (width < 750) {
+        return obj.iphone;
+
+    } else if (width >= 750 && width <= 1024) {
+        if (width <= 900) {
+            return obj.ipad.p;
+        } else {
+            return obj.ipad.l;
+        }
+
+    } else {
+        return obj.desktop;
+    }
+}
+
+$(window).resize(function () {
+    $.each(pageNames, function (idx, obj) {
+        if (obj.name == qpage) {
+            document.getElementById('cmd_iframe').height = findDevice(obj.device);
+            return false;
+        }
+    });
+});
+
